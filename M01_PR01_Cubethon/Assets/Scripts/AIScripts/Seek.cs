29f9599e0b41a44c35b92d11e2e51074d60abf82
Garrett Smith <g.smith61900@gmail.com>
2021-01-24 12:46:52 -0700

    Initial Commit

---
+++ M01_PR01_Cubethon/Assets/Scripts/AIScripts/Seek.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Seek : MonoBehaviour
+{
+    public Transform target;
+
+    public float maxSpeed = 5f;
+
+    void Update()
+    {
+        KinematicSteeringOutput steeringOutput = GetSteeringOutput();
+        transform.position += steeringOutput.velocity * Time.deltaTime;
+    }
+
+    public KinematicSteeringOutput GetSteeringOutput()
+    {
+        KinematicSteeringOutput result = new KinematicSteeringOutput();
+
+        result.velocity = target.position - this.transform.position;
+
+        result.velocity.Normalize();
+        result.velocity *= maxSpeed;
+
+        float orientationAngle = newOrientation(transform.eulerAngles.y, result.velocity);
+        this.transform.eulerAngles = new Vector3(0, orientationAngle * (180 / Mathf.PI), 0);
+
+        result.rotation = 0;
+        return result;
+    }
+
+    public float newOrientation(float currOrAngle, Vector3 vel)
+    {
+        if (vel.magnitude > 0)
+            return Mathf.Atan2(vel.x, vel.z);
+        else
+            return currOrAngle;
+
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # GS_M01_PR01_SeekAndFlee
  
+Link to playable: 
